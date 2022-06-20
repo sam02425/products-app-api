b6cdb7ce2d39e868a092eaacaaa8358a786e91ea
sam.02425 <saumilpatel2425@gmail.com>
2022-06-19 23:21:47 -0500

    serializers, view , url for user app created

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,7 +17,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +27,6 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
-    )
+    ),
+    path('api/user/', include('user.urls')),
 ]
